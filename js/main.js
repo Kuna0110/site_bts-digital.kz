5470303b89c9ae45c4c0d1cdb05b1f72b1b8a952
Your NameKuna0110 <sardar.kuna@gmail.com>
2021-12-11 00:15:47 +0600

    11.12.2021

--- js/main.js
+++ js/main.js
@@ -7,3 +7,20 @@ if (navButton) {
         navButton.classList.toggle('_active');
     });
 }
+//Первый слайдер сдвиг влево
+const buttonPrev = document.querySelector('.slick-arrow-prev');
+if (buttonPrev) {
+    const slickSliderPrev = document.querySelector('.slick-box');
+    buttonPrev.addEventListener('click', function(e) {
+        slickSliderPrev.classList.toggle('prev');
+    });
+}
+
+//Первый слайдер сдвиг вправо
+const buttonNext = document.querySelector('.slick-arrow-next');
+if (buttonNext) {
+    const slickSliderPrev = document.querySelector('.slick-box');
+    buttonNext.addEventListener('click', function(e) {
+        slickSliderPrev.classList.toggle('next');
+    });
+}
